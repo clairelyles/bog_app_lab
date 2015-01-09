365669ab08aa97e67d1318a146882956af08e163
clairelyles <claire.lyles@gmail.com>
2015-01-09 12:08:51 -0800

    added a form to New view

--- bog/app/controllers/creatures_controller.rb
+++ bog/app/controllers/creatures_controller.rb
@@ -4,20 +4,19 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
   end
 
-  def show
-    @creature = Creature.find(params[:id])
-  end
-
   def new
-
+    @creature = Creature.new
   end
 
   def create
-    @creature = Creature.new(params.require(:creature).permit(:name, :desc))
-    creature = Creature.create(creature)
-    redirect_to = "/creatures"
+    render json: params
+    # @creature = Creature.new(params.require(:creature).permit(:name, :desc))
+    # creature = Creature.create(creature)
+    # redirect_to = "/creatures"
   end
 
-
+  def show
+    @creature = Creature.find(params[:id])
+  end
 
 end
